9ddcb5e5b5e2b3bd63d36c00a2ba30bdaea81a1b
TaeHyoungKwon <kwon5604@naver.com>
2017-10-28 15:26:31 +0900

    Add star_ordering_list in views.py and Make star_ordering template and routing

--- taehyung/backend_demo/app/urls.py
+++ taehyung/backend_demo/app/urls.py
@@ -2,7 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    
+    #Lists
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^ordered_star/$', views.post_list_ordered_star, name='post_list_ordered_star'),    
+    
     url(r'^reply/new/$', views.reply_new, name='reply_new'),
     url(r'^corporation/new/$', views.corporation_new, name='corporation_new'),
+
+ 
 ]
--- taehyung/backend_demo/app/views.py
+++ taehyung/backend_demo/app/views.py
@@ -9,15 +9,37 @@ from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     qs = Foods.objects.all()
+
     qs_reply = Reply.objects.all()
     qs_corporation = Corporation.objects.all()
+    
 
     context={
         "foods_list" : qs,
         "reply_list" : qs_reply,
-        "corporation_list" : qs_corporation,
+        "corporation_list":qs_corporation
     }
     return render(request,"app/index.html",context)
+
+
+def post_list_ordered_star(request):
+    
+    qs_star = Foods.objects.all().order_by('-star')
+
+    qs_reply = Reply.objects.all()
+    qs_corporation = Corporation.objects.all()
+
+    context={   
+        
+        "star_ordered_list" : qs_star,
+        "reply_list" : qs_reply,
+        "corporation_list":qs_corporation
+    }
+    return render(request,"app/ordered_star.html",context)
+
+
+
+#Reply Creating and Corporation Creating
 
 class ReplyCreateView(CreateView):
     model = Reply
@@ -30,9 +52,6 @@ class ReplyCreateView(CreateView):
         reply.save()
         return super(ReplyCreateView, self).form_valid(form)
 
-reply_new = login_required(ReplyCreateView.as_view(model=Reply,form_class=ReplyForm,template_name = 'app/add_reply.html'))
-
-
 class CorporationCreateView(CreateView):
     model = Corporation
     form_class = CorporationForm
@@ -44,5 +63,8 @@ class CorporationCreateView(CreateView):
         corporation.save()
         return super(CorporationCreateView, self).form_valid(form)
 
+
+reply_new = login_required(ReplyCreateView.as_view(model=Reply,form_class=ReplyForm,template_name = 'app/add_reply.html'))
 corporation_new = login_required(CorporationCreateView.as_view(model=Corporation,form_class=CorporationForm,template_name = 'app/add_corporation.html'))
+
 
